afcfb3b52d6db57a1970f683cd7194c502d35fc8
Yukiko <yukiko.ikejiri@gmail.com>
2024-05-15 13:58:36 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
 function toggleMenu() {
-    var modal = document.getElementById('modal');
-    if (modal.style.display === 'block') {
-      modal.style.display = 'none';
-    } else {
-      modal.style.display = 'block';
-      populateModal();
-    }
+  var modal = document.getElementById('modal');
+  if (modal.style.display === 'block') {
+    modal.style.display = 'none';
+  } else {
+    modal.style.display = 'block';
+    populateModal();
+  }
 }
-  
+
 function populateModal() {
-    var modalContent = document.getElementById('modalContent');
-    var navbar = document.getElementById('navbar');
+  var modalContent = document.getElementById('modalContent');
+  var navbar = document.getElementById('navbar');
+
+  modalContent.innerHTML = navbar.innerHTML;
   
-    modalContent.innerHTML = navbar.innerHTML;
-    
-    // Attach click event to close modal on link click
-    var links = modalContent.querySelectorAll('.bar-item');
-    links.forEach(function(link) {
-      link.onclick = function() {
-        document.getElementById('modal').style.display = 'none';
-      };
-    });
+  // Attach click event to close modal on link click
+  var links = modalContent.querySelectorAll('.bar-item');
+  links.forEach(function(link) {
+    link.onclick = function() {
+      document.getElementById('modal').style.display = 'none';
+    };
+  });
 }
-  
+
 window.onclick = function(event) {
-    var modal = document.getElementById('modal');
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
+  var modal = document.getElementById('modal');
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
-  
